b5287b034e43eb1ad7799e9a46fad3ba5df38b88
Kai Liu <kailiu@Kais-MacBook-Pro.local>
2018-06-03 19:47:25 -0500

    Add KML layer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,18 @@ import React, { Component } from "react";
 require("ol/ol.css");
 var ol_Map = require("ol/map").default;
 var ol_layer_Tile = require("ol/layer/tile").default;
+var ol_layer_Vector = require("ol/layer/vector").default;
 var ol_source_OSM = require("ol/source/osm").default;
+var ol_source_Vector = require("ol/source/vector").default;
+var ol_format_KML = require("ol/format/kml").default;
 var ol_View = require("ol/view").default;
+
+var placeLayer = new ol_layer_Vector({
+  source: new ol_source_Vector({
+    url: "https://s3.amazonaws.com/climateathomekml/KML_Samples.kml",
+    format: new ol_format_KML()
+  })
+});
 
 class App extends Component {
   constructor(props) {
@@ -19,7 +29,8 @@ class App extends Component {
       layers: [
         new ol_layer_Tile({
           source: new ol_source_OSM()
-        })
+        }),
+        placeLayer
       ],
       view: new ol_View({
         center: [0, 0],
